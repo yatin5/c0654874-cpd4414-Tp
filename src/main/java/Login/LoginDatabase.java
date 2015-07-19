23ddc034f4fac7fea5984a2102864d5a5f7f0a12
yatin <yatin_3313@hotmail.com>
2015-07-19 14:59:32 -0400

    Login Page Created

---
+++ src/main/java/Login/LoginDatabase.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author HP
+ */
+public class LoginDatabase {
+    
+      public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbc = "jdbc:mysql://localhost/c0654874";
+            String user = "root";
+            String pass = "";
+            conn = DriverManager.getConnection(jdbc, user, pass);
+            String query = "SELECT * FROM login";
+
+        } catch (ClassNotFoundException  ex) {
+            System.err.println("No class found Exception" + ex.getMessage());
+        }
+        return conn;
+    }
+    
+}
