a82ed315042f1ab13b6fa2a9adb75a253dbaf0d8
yatin <yatin_3313@hotmail.com>
2015-07-23 01:28:38 -0400

    login clas check

--- src/main/java/main/Login.java
+++ src/main/java/main/Login.java
@@ -1,70 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package main;
 
 import static main.LoginDatabase.getConnection;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+/**
+ *
+ * @author HP
+ */
 public class Login extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-	  Connection conn = null;	
-            
-            try {
-                conn = getConnection();
-            } catch (SQLException ex) {
-                System.out.println("not getting connection");
-            }
-            
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+       protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            String name = request.getParameter("user");
+            String pass = request.getParameter("pass");
             Statement s = null;
+            String id= null;
+            String id2 = null;
+            String query = "select id,id2 from login where userid = "+name+" and password = "+pass+"";
+            ResultSet set = s.executeQuery(query);
             
-            try {
-                s = conn.createStatement();
-            } catch (SQLException ex) {
-                System.out.println("ststement is not processing");
+            while(set.next()){
+                id = set.getString("id");
+                id2 = set.getString("id2");
             }
-             
-String user = req.getParameter("user"); 
-String pass = req.getParameter("pass"); 
-String id=null; String userId=null; String sql = "SELECT id, userId, password FROM login where username='"+user+"' and password='"+pass+"'"; 
-ResultSet rs = null; 
-            try {
-                rs = s.executeQuery(sql);
-            } catch (SQLException ex) {
-                System.out.println("no query found");
-            }
-            try {
-                while(rs.next()) { id=rs.getString("id"); userId=rs.getString("userId"); }
-            } catch (SQLException ex) {
-                System.out.println("no id exixt");
-            }
+               if(id != null) {
+                   System.out.println("Login Successful");
+               } else{
+                   System.out.println("Login Failed");
+               }
+            
+            
+            
+//            if( name.equalsIgnoreCase(name) && pass.equalsIgnoreCase(pass)){
+//                
+//                HttpSession session= request.getSession();
+//                
+//                session.setAttribute("xxx", name);
+//                session.setAttribute("yyy", pass);
+//                
+//                RequestDispatcher rd = request.getRequestDispatcher("index.html");
+//                
+//                rd.forward(request, response);
+//            }
+//            else {
+//               RequestDispatcher rd = request.getRequestDispatcher("login.html");
+//               rd.include(request, response);
+//            }
+        } finally{
+            out.close();
+        }
+    }
 
- if(id!=null) {
-System.out.println("Login Success"); 
-}else {
-System.out.println("Login Failed");
-}
-	}
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+           try {
+               processRequest(request, response);
+           } catch (SQLException ex) {
+               Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+           }
+    }
 
-	private void response(HttpServletResponse resp, String msg)
-			throws IOException {
-		PrintWriter out = resp.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<t1>" + msg + "</t1>");
-		out.println("</body>");
-		out.println("</html>");
-	}
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+           try {
+               processRequest(request, response);
+           } catch (SQLException ex) {
+               Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+           }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
